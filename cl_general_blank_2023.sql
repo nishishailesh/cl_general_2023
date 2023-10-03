3b3e9df4d5048ec00b81216a8833e41f22a953c0
Your Name <you@example.com>
2023-10-03 10:54:54 +0530

    ok

--- cl_general_blank_2023.sql
+++ cl_general_blank_2023.sql
@@ -1719,4 +1719,4 @@ CREATE TABLE `xxx_lab_reference_value` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-03 10:54:29
+-- Dump completed on 2023-10-03 10:54:54
